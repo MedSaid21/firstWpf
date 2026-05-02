b35d7bb7dc76c4f522ba0af31253127d32d54846
EL GORRI MOHAMMED SAID <159718186+MedSaid21@users.noreply.github.com>
2026-05-02 05:38:41 +0100

    first commit with grid view

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace firstWPF
+{
+
+    public partial class MainWindow : Window
+    {
+        bool running = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
